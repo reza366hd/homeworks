addce7536a8e07ec4f9a54e6ea36a3dbab7b678e
Reza86Mirebrahimi <141235424+Reza86Mirebrahimi@users.noreply.github.com>
2023-08-06 12:42:42 +0330

    practice of section 11

---
+++ 11/student.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	var student []string
+	// Out:
+	for {
+		fmt.Println("do you have any student? (inter no for end)")
+		fmt.Scanln(&s)
+
+		// switch s {
+		// case "no":
+		// 	break Out
+
+		// default:
+		// 	student = append(student, s)
+		// }
+
+		if s == "no" {
+			break
+		}
+		student = append(student, s)
+
+	}
+	fmt.Println(student)
+}
