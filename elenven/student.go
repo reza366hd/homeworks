cdd3f69da040e3311666ab2a3048559ce68fb702
Reza86Mirebrahimi <141235424+Reza86Mirebrahimi@users.noreply.github.com>
2023-08-30 13:00:30 +0330

    packages update

--- elenven/student.go
+++ elenven/student.go
@@ -5,18 +5,10 @@ import "fmt"
 func eleven() {
 	var s string
 	var student []string
-	// Out:
+
 	for {
 		fmt.Println("do you have any student? (inter no for end)")
 		fmt.Scanln(&s)
-
-		// switch s {
-		// case "no":
-		// 	break Out
-
-		// default:
-		// 	student = append(student, s)
-		// }
 
 		if s == "no" {
 			break
